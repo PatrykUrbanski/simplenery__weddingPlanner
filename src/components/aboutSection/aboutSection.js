3f643acd65af672375f7f975449a98bd2c92b4ce
Patryk Urbanski <pat.urbanski@gmail.com>
2020-06-19 12:45:38 +0200

    build aboutSection strucutre

--- src/components/aboutSection/aboutSection.js
+++ src/components/aboutSection/aboutSection.js
@@ -5,24 +5,43 @@ export const About = () => {
         <>
           <section className={"about"}>
               <div className={"about__content container"}>
-                  <div className={"card"}>
-                      <h2 className={"card__title"}>Your truly personal</h2>
-                      <h3 className={"card__subtitle"}>Wedding planner</h3>
-                      <p className={"card__text"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci blanditiis ea expedita fugiat id qui reprehenderit velit. Accusamus corporis doloribus dolorum ex laboriosam modi officia possimus sapiente soluta velit?</p>
-                      <a href={"#"} className={"card__link"}>content</a>
+                  <div className={"cardImageSection"}>
+                      <div className={"card"}>
+                          <h2 className={"card__title"}>Your truly personal</h2>
+                          <h3 className={"card__subtitle"}>Wedding planner</h3>
+                          <p className={"card__text"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci blanditiis ea expedita fugiat id qui reprehenderit velit. Accusamus corporis doloribus dolorum ex laboriosam modi officia possimus sapiente soluta velit?</p>
+                          <a href={"#"} className={"card__link"}>Discover our recent works.</a>
+                      </div>
+
+                      <div className={"images"}>
+                          <div className={"images__elem cutlery"} />
+                          <div className={"images__elem candles"} />
+                          <div className={"images__elem table"} />
+                      </div>
                   </div>
-                  <div className={"about__content__image"} />
-                  <div className={"card"}>
-                      <h2 className={"card__title"}>Your truly personal</h2>
-                      <h3 className={"card__subtitle"}>Wedding planner</h3>
-                      <p className={"card__text"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci blanditiis ea expedita fugiat id qui reprehenderit velit. Accusamus corporis doloribus dolorum ex laboriosam modi officia possimus sapiente soluta velit?</p>
-                      <a href={"#"} className={"card__link"}>content</a>
+                  <div className={"cardImageSection"}>
+                      <div className={"card"}>
+                          <h2 className={"card__title"}>Made specially for you</h2>
+                          <h3 className={"card__subtitle"}>Unique decorations</h3>
+                          <p className={"card__text"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci blanditiis ea expedita fugiat id qui reprehenderit velit. Accusamus corporis doloribus dolorum ex laboriosam modi officia possimus sapiente soluta velit?</p>
+                          <a href={"#"} className={"card__link"}>content</a>
+                      </div>
+                      <div className={"images"}>
+                          <div className={"images__elem flowers"} />
+                          <div className={"images__elem window"} />
+                      </div>
                   </div>
-                  <div className={"card"}>
-                      <h2 className={"card__title"}>Your truly personal</h2>
-                      <h3 className={"card__subtitle"}>Wedding planner</h3>
-                      <p className={"card__text"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci blanditiis ea expedita fugiat id qui reprehenderit velit. Accusamus corporis doloribus dolorum ex laboriosam modi officia possimus sapiente soluta velit?</p>
-                      <a href={"#"} className={"card__link"}>Have a look at our portfolio</a>
+                  <div className={"cardImageSection"}>
+                      <div className={"card"}>
+                          <h2 className={"card__title"}>Your truly personal</h2>
+                          <h3 className={"card__subtitle"}>Wedding planner</h3>
+                          <p className={"card__text"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci blanditiis ea expedita fugiat id qui reprehenderit velit. Accusamus corporis doloribus dolorum ex laboriosam modi officia possimus sapiente soluta velit?</p>
+                          <a href={"#"} className={"card__link"}>Have a look at our portfolio</a>
+                      </div>
+                      <div className={"images"}>
+                          <div className={"images__elem cutlery"} />
+                          <div className={"images__elem candles"} />
+                      </div>
                   </div>
               </div>
           </section>
