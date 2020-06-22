e75751f57ba165a1de0296c24224c4869d53e90d
Patryk Urbanski <pat.urbanski@gmail.com>
2020-06-22 12:44:05 +0200

    added header scroll logic

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,28 +1,44 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {useMediaQuery} from 'react-responsive';
 
 export const Header = () => {
+    const isMobile = useMediaQuery({query: "(max-device-width: 768px)"});
     const [openMenu, setOpenMenu] = useState(false);
+    const [headerOnTop, setHeaderOnTop] = useState(true);
 
     const handleOpenMenu = () => {
         setOpenMenu(!openMenu)
     };
 
+    useEffect(() => {
+        const headerHandler = window.addEventListener("scroll", () => {
+              window.pageYOffset !== 0 ? setHeaderOnTop(false) : setHeaderOnTop(true)
+        })
+    }, []);
+
+
+
+
+
+
     return (
         <>
-            <header className={"header"}>
+            <header className={`header ${headerOnTop ? false : "markHeader"}`}>
                 <div className={"header__content container"}>
                     <img className={"logo"} alt={"logo"} src={"/images/logo.svg"} />
-                    <div className={"header__content__menuIcon"} onClick={handleOpenMenu} style={{color: `${openMenu ? "white" : "black"}`}}>
+                    <div className={"header__content__menuIcon"} onClick={handleOpenMenu} >
                         <span className={"header__content__menuIcon__elem"} style={{opacity: `${openMenu ? "0" : "1"}`}}/>
                         <span className={"header__content__menuIcon__elem"} style={{transform: `${openMenu ? "translate(0%, 175%) rotate(-45deg)" : "none"}`}}/>
                         <span className={"header__content__menuIcon__elem"} style={{transform: `${openMenu ? "rotate(45deg)" : "none"}`}}/>
                         <span className={"header__content__menuIcon__elem"} style={{opacity: `${openMenu ? "0" : "1"}`}}/>
                     </div>
-                    <div className={"menu"}>
-                        <a className={"menu__elem"} href={"#"}>Content</a>
-                        <a className={"menu__elem"} href={"#"}>Content</a>
-                        <a className={"menu__elem"} href={"#"}>Content</a>
-                        <a className={"menu__elem"} href={"#"}>Content</a>
+                    <div className={"menu"} style={{height: `${isMobile ? openMenu ? "100vh" : "0" : "auto"}`}}>
+                        <div className={"bgLine"}/>
+                        <a className={"menu__elem"} href={"#"} onClick={handleOpenMenu}>Home</a>
+                        <a className={"menu__elem"} href={"#"} onClick={handleOpenMenu}>Our story</a>
+                        <a className={"menu__elem"} href={"#"} onClick={handleOpenMenu}>Our works</a>
+                        <a className={"menu__elem"} href={"#"} onClick={handleOpenMenu}>Contact us</a>
+                        <div className={"bgLine"}/>
                     </div>
                 </div>
             </header>
